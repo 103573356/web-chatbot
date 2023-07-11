5899cac2b325620b9e1897d7f9c303d7faa4c44b
Hoàng Cao Minh <hoangcaominh1812@gmail.com>
2023-07-11 21:14:11 +0700

    Update google sheet script url

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ $(() => {
             return;
 
         // Submit form to Google Sheet
-        let action = "https://script.google.com/macros/s/AKfycby7QPqjcpl6m6bFFFsMuqnBdXm6FIlm-N1wDmXt1QbDXdAnbtrzH6nvNvLwuiIFzgwBFA/exec";
+        let action = "https://script.google.com/macros/s/AKfycbzupHE4ttsXfsveeAGmRzjo9XapnhN7teJJ3TrWDl9oKKj5NQfUm1Ne1YTK64dvBirBTw/exec";
         $.post(action, $("#popupForm").serialize(), (data, status, xhr) => {
         });
         
